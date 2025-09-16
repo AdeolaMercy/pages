cf3c53c5e3f70a0ae4188578c52cf442b5215178
AdeolaMercy <mercycharis44@gmail.com>
2025-09-16 11:57:06 +0100

    Update join_domain.ps1

--- join_domain.ps1
+++ join_domain.ps1
@@ -1,8 +1,5 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string]$DomainName,
-
-    [Parameter(Mandatory=$true)]
     [string]$DomainUser,
 
     [Parameter(Mandatory=$true)]
@@ -14,4 +11,4 @@ $SecurePassword = ConvertTo-SecureString $DomainUserPassword -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential("$DomainName\$DomainUser", $SecurePassword)
 
 Write-Output "Joining computer to domain $DomainName ..."
-Add-Computer -DomainName $DomainName -Credential $Credential -Force -Restart
+Add-Computer -DomainName test.local -Credential $Credential -Force -Restart
