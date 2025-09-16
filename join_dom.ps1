5a3104ffd35e27630d798ab25e35d189914f9479
AdeolaMercy <mercycharis44@gmail.com>
2025-09-16 12:23:30 +0100

    Create join_dom.ps1

---
+++ join_dom.ps1
@@ -0,0 +1,14 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$DomainUser,
+
+    [Parameter(Mandatory=$true)]
+    [string]$DomainUserPassword
+)
+
+# Convert password to SecureString
+$SecurePassword = ConvertTo-SecureString $DomainUserPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential("test.local\$DomainUser", $SecurePassword)
+
+Write-Output "Joining computer to domain..."
+Add-Computer -DomainName test.local -Credential $Credential -Force -Restart
